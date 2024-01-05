eb31fcf2d27b947ce7286faefb26224ff4f8504d
Zoran Tošev <zoran.tosev@symphony.is>
2024-01-05 11:22:07 +0100

    layer picker setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import mapConfig from "./layerPickerConfig.json";
+import { LayerPicker, createMap } from "webcore-map-nextgen";
 
 function App() {
+  const [mapInstance, setMapInstance] = useState(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRefreshEnabled, setIsRefreshEnabled] = useState(false);
+
+  const toggleModal = () => setIsOpen((oldIsOpen) => !oldIsOpen);
+
+  useEffect(() => {
+    createMap({ mapConfig }).then(setMapInstance);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <LayerPicker map={mapInstance} />
         <a
           className="App-link"
           href="https://reactjs.org"
